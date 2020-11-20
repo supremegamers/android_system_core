47fa70a134bbf734a5e4c5b04e18ca876f0425d7
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-11-20 11:34:54 -0300

    init: Weaken property override security only when spoofing safetynet
    Change-Id: I740afaa27de82bec1e6d58b58d431141ca6b4e3f
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -840,6 +840,9 @@ static const char *snet_prop_value[] = {
 };
 
 static void workaround_snet_properties() {
+    // Weaken property override security to set safetynet props
+    weaken_prop_override_security = true;
+
 	std::string error;
 	LOG(INFO) << "snet: Hiding sensitive props";
 
@@ -847,6 +850,9 @@ static void workaround_snet_properties() {
 	for (int i = 0; snet_prop_key[i]; ++i) {
 		PropertySet(snet_prop_key[i], snet_prop_value[i], &error);
 	}
+
+    // Restore the normal property override security after safetynet props have been set
+    weaken_prop_override_security = false;
 }
 
 // If the ro.product.[brand|device|manufacturer|model|name] properties have not been explicitly
@@ -1152,9 +1158,6 @@ void PropertyLoadBootDefaults() {
         }
     }
 
-    // Weaken property override security during execution of the vendor init extension
-    weaken_prop_override_security = true;
-
     // Update with vendor-specific property runtime overrides
     vendor_load_properties();
 
@@ -1164,13 +1167,10 @@ void PropertyLoadBootDefaults() {
     property_derive_legacy_build_fingerprint();
     property_initialize_ro_cpu_abilist();
 
+    update_sys_usb_config();
+
     // Workaround SafetyNet
     workaround_snet_properties();
-
-    // Restore the normal property override security after init extension is executed
-    weaken_prop_override_security = false;
-
-    update_sys_usb_config();
 }
 
 bool LoadPropertyInfoFromFile(const std::string& filename,

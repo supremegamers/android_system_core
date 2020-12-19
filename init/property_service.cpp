66064f8c5eef451dac5f6150de5d918066aca1d8
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2020-12-19 15:57:27 +0000

    Revert "init: Add vendor-specific initialization hooks."
    This reverts commit 7bc78d1354cd2e8df1ba693c627d5be39206e375.
    
    Change-Id: I0c3241ef3626bc989b34723e30b2339e4a62399d

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -71,7 +71,6 @@
 #include "subcontext.h"
 #include "system/core/init/property_service.pb.h"
 #include "util.h"
-#include "vendor_init.h"
 
 using namespace std::literals;
 
@@ -899,9 +898,6 @@ void PropertyLoadBootDefaults() {
                        << "' while loading .prop files" << error;
         }
     }
-
-    // Update with vendor-specific property runtime overrides
-    vendor_load_properties();
 
     property_initialize_ro_product_props();
     property_derive_build_fingerprint();

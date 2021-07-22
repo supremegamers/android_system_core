099518e2f383e2a4cb1c1fcdaef0b18c7c907c2e
nebrassy <nebras30@gmail.com>
2021-07-23 00:26:21 +0200

    return error when attempting to merge from source slot
    Change-Id: I34cac7eed9210ec9ebe2c051d20bcd0fe29f3a91

--- fs_mgr/libsnapshot/snapshot.cpp
+++ fs_mgr/libsnapshot/snapshot.cpp
@@ -3505,7 +3505,8 @@ UpdateState SnapshotManager::ProcessUpdateStateOnDataWipe(bool allow_forward_mer
                 LOG(ERROR) << "Reverting to old slot since update will be deleted.";
                 device_->SetSlotAsUnbootable(slot_number);
             } else {
-                LOG(INFO) << "Booting from " << slot << " slot, no action is taken.";
+                LOG(ERROR) << "Booting from " << slot << " slot, no action is taken.";
+                return UpdateState::MergeFailed;
             }
             break;
         }

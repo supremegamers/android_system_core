647a0cca1f19554414d9f6009fa8142be66037e4
hmtheboy154 <buingoc67@gmail.com>
2024-01-08 11:53:12 -0500

    Force not to build init_first_stage
    As BOARD_BUILD_SYSTEM_ROOT_IMAGE is being deprecated and we don't
    have any alternative, we need to remove init_first_stage manually
    to avoid conflict with "init: handle ctrl-alt-del event"
    
    Change-Id: Ieb66fd064768cc80722566211481a0a8b5c5ee93
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- init/Android.mk
+++ init/Android.mk
@@ -8,9 +8,4 @@ LOCAL_MODULE := init_vendor
 LOCAL_LICENSE_KINDS := SPDX-license-identifier-Apache-2.0
 LOCAL_LICENSE_CONDITIONS := notice
 LOCAL_NOTICE_FILE := $(LOCAL_PATH)/NOTICE
-ifneq ($(BOARD_USES_RECOVERY_AS_BOOT),true)
-LOCAL_REQUIRED_MODULES := \
-   init_first_stage \
-
-endif  # BOARD_USES_RECOVERY_AS_BOOT
 include $(BUILD_PHONY_PACKAGE)
